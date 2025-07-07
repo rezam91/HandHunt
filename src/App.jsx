3ead93389f0b6d8b82839ad48ec318609a5c32c6
Reza <r.mehralizadeh@gmail.com>
2025-07-08 01:27:46 +0330

    feat(Player info): Player info part added to game

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Handhunt from './components/handhunt/index'
 import Logo from './components/general/logo/index'
 import GameNumber from './components/general/gamenumber/index'
 import RefreshButton from './components/general/layout/refresh/refresh'
+import PlayerInfo from './components/general/layout/playerInfo'
+
 import { useEffect, useState } from 'react'
 
 const App = () => {
@@ -14,8 +16,9 @@ const App = () => {
   },[])
   return (
     <>
-      <h1>Welcome to Handhunt Game!</h1>
+      <h1>Handhunt Game!</h1>
       <Logo />
+      <PlayerInfo />
       <GameNumber onChangeNumber={ChangeNumber} />
       <Handhunt gameNum={handNumber} />
       <RefreshButton />
---
+++ src/components/general/layout/playerInfo/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const PlayerInfo = () => {
+    const [playerName,setName] = useState('foo')
+    const [nameVisibility,setVisibility] = useState(false)
+    const [divOpacity,setOpacity] = useState('0')
+    const changeName = (e) => {
+        setName(e.target.value)
+    }
+    const submitPlayerName = (e) => {
+        e.preventDefault()
+        setVisibility(true)
+        setOpacity('1')
+    }
+    return (
+        <div style={{position:'relative'}}>
+            <form style={{visibility: !nameVisibility ? 'visible' : 'hidden'}} onSubmit={submitPlayerName} className="player--info">
+                <span>Player's name: </span>
+                <input placeholder="Please insert your name" type="text" onChange={changeName} />
+                <button type="submit" className="ok--button">OK</button>
+            </form>
+                <div style={{top:'0',transform:'translateX(-50%)',left: '50%',position:'absolute',fontSize: '20px', opacity:divOpacity, transition: 'all 1s ease-in-out',visibility: !nameVisibility ? 'hidden' : 'visible'}}>Welcome <strong>{playerName}</strong>!</div>
+        </div>
+    )
+}
+
+export default PlayerInfo
