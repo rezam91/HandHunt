fdc5b2126b54d22d768bf626517fbe9053cddac0
Reza <r.mehralizadeh@gmail.com>
2025-07-08 01:52:46 +0330

    fix(player name): player cant submit empty name as his or her name

--- src/components/general/layout/playerInfo/index.jsx
+++ src/components/general/layout/playerInfo/index.jsx
@@ -4,8 +4,12 @@ const PlayerInfo = () => {
     const [playerName,setName] = useState('foo')
     const [nameVisibility,setVisibility] = useState(false)
     const [divOpacity,setOpacity] = useState('0')
+    const [buttonVisibility,setButtonVisibility] = useState(true)
+
     const changeName = (e) => {
         setName(e.target.value)
+        e.target.value.length ? setButtonVisibility(false) : setButtonVisibility(true)
+
     }
     const submitPlayerName = (e) => {
         e.preventDefault()
@@ -17,9 +21,9 @@ const PlayerInfo = () => {
             <form style={{visibility: !nameVisibility ? 'visible' : 'hidden'}} onSubmit={submitPlayerName} className="player--info">
                 <span>Player's name: </span>
                 <input placeholder="Please insert your name" type="text" onChange={changeName} />
-                <button type="submit" className="ok--button">OK</button>
+                <button disabled={buttonVisibility} type="submit" className="ok--button">OK</button>
             </form>
-                <div style={{top:'0',transform:'translateX(-50%)',left: '50%',position:'absolute',fontSize: '20px', opacity:divOpacity, transition: 'all 1s ease-in-out',visibility: !nameVisibility ? 'hidden' : 'visible'}}>Welcome <strong>{playerName}</strong>!</div>
+                <div style={{top:'0',transform:'translateX(-50%)',left: '50%',position:'absolute',fontSize: '20px', opacity:divOpacity, transition: 'all 1s ease-in-out',visibility: !nameVisibility ? 'hidden' : 'visible'}}>-- Welcome <strong>{playerName}</strong>! --</div>
         </div>
     )
 }
